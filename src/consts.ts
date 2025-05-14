2eaabfa8f48f066c76d145ea6f883ed4dbf5082a
Emmanuel Millan <emmanueljmillan@outlook.com>
2025-05-14 17:15:46 -0400

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,22 +2,26 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "Emmanuel Millan",
+  DESCRIPTION: "Welcome to my Portfolio! Template from AstroSphere, developed by Mark Horn.",
+  AUTHOR: "Emmanuel Millan",
 }
 
-// Work Page
+// Work Page i.e. Experience
 export const WORK: Page = {
-  TITLE: "Work",
+  TITLE: "Experience",
   DESCRIPTION: "Places I have worked.",
 }
 
+//Removing Blog
+/*
 // Blog Page
 export const BLOG: Page = {
   TITLE: "Blog",
   DESCRIPTION: "Writing on topics I am passionate about.",
 }
+*/
+//Ending comment
 
 // Projects Page 
 export const PROJECTS: Page = {
@@ -41,13 +45,16 @@ export const LINKS: Links = [
     TEXT: "Experience", 
     HREF: "/Experience", 
   },
-  <!-- Removing blog -->
-  <!-- 
+  
+//Removing Blog
+/*
   { 
     TEXT: "Blog", 
     HREF: "/blog", 
   },
-      -->
+*/  
+//Ending comment
+  
   { 
     TEXT: "Projects", 
     HREF: "/projects", 
@@ -59,26 +66,20 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "emmanueljmillan@outlook.com",
+    HREF: "emmanueljmillan@outlook.com",
   },
   { 
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "emmanuel-dev",
+    HREF: "https://github.com/emmanueljmillan"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
-  },
-  { 
-    NAME: "Twitter",
-    ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
+    TEXT: "emmanuel-dev",
+    HREF: "https://www.linkedin.com/in/emmanueljmillan/",
   },
 ]
 
