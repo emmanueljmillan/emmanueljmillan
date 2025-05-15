39e9311b4fba6765a10b1c5030ae07aa06cbc184
Emmanuel Millan <emmanueljmillan@outlook.com>
2025-05-14 22:08:52 -0400

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -67,7 +67,7 @@ export const SOCIALS: Socials = [
     NAME: "Email",
     ICON: "email", 
     TEXT: "emmanueljmillan@outlook.com",
-    HREF: "malito:emmanueljmillan@outlook.com",
+    HREF: "mailto:emmanueljmillan@outlook.com",
   },
   { 
     NAME: "Github",
